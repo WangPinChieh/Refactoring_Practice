a7d8a23f4581a65b41d25c51fced6610361868d9
Jay <jay.wang@titansoft.com.sg>
2020-05-25 14:39:44 +0800

    refactor amount and frequent renter points

--- Refactoring_Practice/Customer.cs
+++ Refactoring_Practice/Customer.cs
@@ -29,36 +29,16 @@ namespace Refactoring_Practice
 			var result = "Rental Record for " + getName() + "\n";
 			foreach (var rental in _rentals)
 			{
-				double thisAmount = 0;
 				//determine amounts for each line
-				switch (rental.getMovie().getPriceCode())
-				{
-					case Movie.REGULAR:
-						thisAmount += 2;
-						if (rental.getDaysRented() > 2)
-							thisAmount += (rental.getDaysRented() - 2) * 1.5;
-						break;
-					case Movie.NEW_RELEASE:
-						thisAmount += rental.getDaysRented() * 3;
-						break;
-					case Movie.CHILDRENS:
-						thisAmount += 1.5;
-						if (rental.getDaysRented() > 3)
-							thisAmount += (rental.getDaysRented() - 3) * 1.5;
-						break;
-				}
 
 				// add frequent renter points
-				frequentRenterPoints++;
-				// add bonus for a two day new release rental
-				if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-				    &&
-				    rental.getDaysRented() > 1) frequentRenterPoints++;
+				frequentRenterPoints += rental.GetFrequentRenterPoints();
+
 				//show figures for this rental
 				result += "\t" + rental.getMovie().getTitle() + "\t" +
-				          thisAmount + "\n";
+				          rental.GetCharge() + "\n";
 
-				totalAmount += thisAmount;
+				totalAmount += rental.GetCharge();
 			}
 			//add footer lines
 			result += "Amount owed is " + totalAmount + "\n";
--- Refactoring_Practice/Program.cs
+++ Refactoring_Practice/Program.cs
@@ -7,7 +7,9 @@ namespace Refactoring_Practice
 		static void Main(string[] args)
 		{
 			var customer = new Customer("Jay");
-			customer.addRental(new Rental(new Movie("Happy Friday",Movie.CHILDRENS), 20));
+			customer.addRental(new Rental(new Movie("Happy Friday", Movie.REGULAR), 20));
+			customer.addRental(new Rental(new Movie("Happy Friday", Movie.NEW_RELEASE), 20));
+
 			Console.WriteLine(customer.GetStatement());
 			Console.ReadLine();
 		}
